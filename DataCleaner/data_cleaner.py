9a97f28bfaa51179702be5ff37c4bbcb7f4ea678
zoobereq <simon@zuberek.net>
2022-04-03 18:22:53 -0400

    Replaced write() with print() and updated RegEx

--- DataCleaner/data_cleaner.py
+++ DataCleaner/data_cleaner.py
@@ -3,18 +3,19 @@
 import argparse
 import re
 
-def text_cleaner(input_file_path: str, output_file_path:str) -> None:
+def text_cleaner(input_file_path:str, output_file_path:str) -> None:
     with open(input_file_path, 'r') as source, open(output_file_path, 'w') as sink:
         for line in source:
-            line = re.sub(r'[^\w\s]', '', line).casefold()
-            sink.write(line)
+            line = line.casefold().strip()
+            line = re.sub(r'[^A-Za-z0-9 ]+', '', line)
+            print(line, file = sink)
 
 def main(args: argparse.Namespace) -> None:
     text_cleaner(args.input, args.output)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help = "Path for input file")
-    parser.add_argument("output", help = "Path for the output file")
+    parser.add_argument("input", help="path to input file")
+    parser.add_argument("output", help="path to output file")
     main(parser.parse_args())
 
